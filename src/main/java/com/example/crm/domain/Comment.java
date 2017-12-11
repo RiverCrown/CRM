791ee0e4a5660c242aae6d047a3240a988123102
RiverCrown <1290021464@qq.com>
2017-12-11 22:08:17 +0800

    compile hibernate-java8 to make LocalDateTime fit with jpa

--- src/main/java/com/example/crm/domain/Comment.java
+++ src/main/java/com/example/crm/domain/Comment.java
@@ -1,6 +1,7 @@
 package com.example.crm.domain;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
@@ -10,7 +11,7 @@ public class Comment {
     @Id
     @GeneratedValue
     private int id;
-    private Date date;
+    private LocalDateTime date;
     private Integer reviewerId;
     private String content;
 
@@ -22,11 +23,11 @@ public class Comment {
         this.id = id;
     }
 
-    public Date getDate() {
+    public LocalDateTime getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(LocalDateTime date) {
         this.date = date;
     }
 
--- src/main/java/com/example/crm/domain/CommentView.java
+++ src/main/java/com/example/crm/domain/CommentView.java
@@ -1,11 +1,12 @@
 package com.example.crm.domain;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class CommentView {
 
     private int id;
-    private Date date;
+    private LocalDateTime date;
     private String reviewerName;
     private String content;
 
@@ -17,11 +18,11 @@ public class CommentView {
         this.id = id;
     }
 
-    public Date getDate() {
+    public LocalDateTime getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(LocalDateTime date) {
         this.date = date;
     }
 
